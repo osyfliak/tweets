0ac0504e1ced7a19adc3742d0931ed970deb5d7f
Osyfliak Ivan <osuflak22@gmail.com>
2023-06-04 23:53:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename='/'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
